88c20d00374ccb419b71f548bd700e6eef722a96
Jiao Jingping <1784922+JiaoJingPing@users.noreply.github.com>
2021-01-02 17:10:19 +0800

    Fixed cached image name

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@ const PRECACHE_LIST = [
   "./js/bootstrap.min.js",
   "./js/hux-blog.min.js",
   "./js/snackbar.js",
-  "./img/avatar-hux.jpg",
+  "./img/avatar-jjp.jpg",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
